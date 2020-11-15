3c7af4a74bd73a5ab15590f9c7c4598275648467
Connor MacKenzie <cmm224@case.edu>
2020-11-15 04:31:03 -0500

    almost perfect

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -16,12 +16,17 @@ for line in lines:
     if len(line) == 0:
         lines.remove(line)
 
+#move the paragraph out of the array and back into a string
 for line in lines:
     data = data + f' {line}'
 
-print(text)
-print(lines)
-print(data)
+
+#debugging
+#print(text)
+#print(lines)
+#print(data)
+
+
 #split on spaces to get words
 words = data.split()
 word_count = len(words)
@@ -32,14 +37,11 @@ letters = 0
 for word in words:
     letters += len(word)
 
-#average_letters = letters/word_count
+average_letters = letters/word_count
 
 #split using re module
-sentences = re.split("(?<=[.!?]) +", data)
-
+sentences = re.split('(?<=[.!?]) +', data)
 sentence_count = len(sentences)
-
-
 
 #counting words again just in case some weird happened up higher
 re_total_words = 0
