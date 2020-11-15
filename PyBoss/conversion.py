a9a1b4b732f061ed29d3a727a6e0c0ab182f6c60
Connor MacKenzie <cmm224@case.edu>
2020-11-15 03:14:09 -0500

    done!

--- PyBoss/conversion.py
+++ PyBoss/conversion.py
@@ -1,83 +1,87 @@
-test = ["100", "Connor MacKenzie", "2020-11-15", "123-45-6789", "Pennsylvania"]
+#this functions read in a list of
+#[employee id, name, dob (year-month-day), ssn, state name]
 
-emp_id = test[0]
-name = test[1]
-dob=test[2]
-ssn=test[3]
-state=test[4]
+#returns [employee id, first name, last name, dob (mm/dd/yyyy), ssn (***-**-1234), state abbrev.]
 
-name_list= name.split()
-first_name = name_list[0]
-last_name = name_list[1]
+def convert_data(employee_data):
 
-dob_list=dob.split("-")
-dob_conv=f'{dob_list[1]}/{dob_list[2]}/{dob_list[0]}'
+    emp_id = employee_data[0]
+    name = employee_data[1]
+    dob=employee_data[2]
+    ssn=employee_data[3]
+    state=employee_data[4]
 
-ssn_list=ssn.split("-")
-ssn_conv=f'***-**-{ssn_list[2]}'
+    name_list= name.split()
+    first_name = name_list[0]
+    last_name = name_list[1]
 
-us_state_abbrev = {
-    'Alabama': 'AL',
-    'Alaska': 'AK',
-    'American Samoa': 'AS',
-    'Arizona': 'AZ',
-    'Arkansas': 'AR',
-    'California': 'CA',
-    'Colorado': 'CO',
-    'Connecticut': 'CT',
-    'Delaware': 'DE',
-    'District of Columbia': 'DC',
-    'Florida': 'FL',
-    'Georgia': 'GA',
-    'Guam': 'GU',
-    'Hawaii': 'HI',
-    'Idaho': 'ID',
-    'Illinois': 'IL',
-    'Indiana': 'IN',
-    'Iowa': 'IA',
-    'Kansas': 'KS',
-    'Kentucky': 'KY',
-    'Louisiana': 'LA',
-    'Maine': 'ME',
-    'Maryland': 'MD',
-    'Massachusetts': 'MA',
-    'Michigan': 'MI',
-    'Minnesota': 'MN',
-    'Mississippi': 'MS',
-    'Missouri': 'MO',
-    'Montana': 'MT',
-    'Nebraska': 'NE',
-    'Nevada': 'NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
-    'Northern Mariana Islands':'MP',
-    'Ohio': 'OH',
-    'Oklahoma': 'OK',
-    'Oregon': 'OR',
-    'Pennsylvania': 'PA',
-    'Puerto Rico': 'PR',
-    'Rhode Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
-    'Tennessee': 'TN',
-    'Texas': 'TX',
-    'Utah': 'UT',
-    'Vermont': 'VT',
-    'Virgin Islands': 'VI',
-    'Virginia': 'VA',
-    'Washington': 'WA',
-    'West Virginia': 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY'
-}
+    dob_list=dob.split("-")
+    dob_conv=f'{dob_list[1]}/{dob_list[2]}/{dob_list[0]}'
 
-state_conv = us_state_abbrev[state]
+    ssn_list=ssn.split("-")
+    ssn_conv=f'***-**-{ssn_list[2]}'
 
-new_list = [emp_id, first_name, last_name, dob_conv, ssn_conv, state_conv]
+    us_state_abbrev = {
+        'Alabama': 'AL',
+        'Alaska': 'AK',
+        'American Samoa': 'AS',
+        'Arizona': 'AZ',
+        'Arkansas': 'AR',
+        'California': 'CA',
+        'Colorado': 'CO',
+        'Connecticut': 'CT',
+        'Delaware': 'DE',
+        'District of Columbia': 'DC',
+        'Florida': 'FL',
+        'Georgia': 'GA',
+        'Guam': 'GU',
+        'Hawaii': 'HI',
+        'Idaho': 'ID',
+        'Illinois': 'IL',
+        'Indiana': 'IN',
+        'Iowa': 'IA',
+        'Kansas': 'KS',
+        'Kentucky': 'KY',
+        'Louisiana': 'LA',
+        'Maine': 'ME',
+        'Maryland': 'MD',
+        'Massachusetts': 'MA',
+        'Michigan': 'MI',
+        'Minnesota': 'MN',
+        'Mississippi': 'MS',
+        'Missouri': 'MO',
+        'Montana': 'MT',
+        'Nebraska': 'NE',
+        'Nevada': 'NV',
+        'New Hampshire': 'NH',
+        'New Jersey': 'NJ',
+        'New Mexico': 'NM',
+        'New York': 'NY',
+        'North Carolina': 'NC',
+        'North Dakota': 'ND',
+        'Northern Mariana Islands':'MP',
+        'Ohio': 'OH',
+        'Oklahoma': 'OK',
+        'Oregon': 'OR',
+        'Pennsylvania': 'PA',
+        'Puerto Rico': 'PR',
+        'Rhode Island': 'RI',
+        'South Carolina': 'SC',
+        'South Dakota': 'SD',
+        'Tennessee': 'TN',
+        'Texas': 'TX',
+        'Utah': 'UT',
+        'Vermont': 'VT',
+        'Virgin Islands': 'VI',
+        'Virginia': 'VA',
+        'Washington': 'WA',
+        'West Virginia': 'WV',
+        'Wisconsin': 'WI',
+        'Wyoming': 'WY'
+    }
 
-print(f'{test}')
-print(f'{new_list}')
+    state_conv = us_state_abbrev[state]
+
+    return_list = [emp_id, first_name, last_name, dob_conv, ssn_conv, state_conv]
+    
+    return return_list
---
+++ PyBoss/main.py
@@ -0,0 +1,107 @@
+import os
+import csv
+
+csvpath = os.path.join("Resources", "employee_data.csv")
+output_file = os.path.join("Resources", "converted_employee_data.csv")
+
+#this functions read in a list of
+#[employee id, name, dob (year-month-day), ssn, state name]
+
+#returns [employee id, first name, last name, dob (mm/dd/yyyy), ssn (***-**-1234), state abbrev.]
+
+def convert_data(employee_data):
+
+    emp_id = employee_data[0]
+    name = employee_data[1]
+    dob=employee_data[2]
+    ssn=employee_data[3]
+    state=employee_data[4]
+
+    name_list= name.split()
+    first_name = name_list[0]
+    last_name = name_list[1]
+
+    dob_list=dob.split("-")
+    dob_conv=f'{dob_list[1]}/{dob_list[2]}/{dob_list[0]}'
+
+    ssn_list=ssn.split("-")
+    ssn_conv=f'***-**-{ssn_list[2]}'
+
+    us_state_abbrev = {
+        'Alabama': 'AL',
+        'Alaska': 'AK',
+        'American Samoa': 'AS',
+        'Arizona': 'AZ',
+        'Arkansas': 'AR',
+        'California': 'CA',
+        'Colorado': 'CO',
+        'Connecticut': 'CT',
+        'Delaware': 'DE',
+        'District of Columbia': 'DC',
+        'Florida': 'FL',
+        'Georgia': 'GA',
+        'Guam': 'GU',
+        'Hawaii': 'HI',
+        'Idaho': 'ID',
+        'Illinois': 'IL',
+        'Indiana': 'IN',
+        'Iowa': 'IA',
+        'Kansas': 'KS',
+        'Kentucky': 'KY',
+        'Louisiana': 'LA',
+        'Maine': 'ME',
+        'Maryland': 'MD',
+        'Massachusetts': 'MA',
+        'Michigan': 'MI',
+        'Minnesota': 'MN',
+        'Mississippi': 'MS',
+        'Missouri': 'MO',
+        'Montana': 'MT',
+        'Nebraska': 'NE',
+        'Nevada': 'NV',
+        'New Hampshire': 'NH',
+        'New Jersey': 'NJ',
+        'New Mexico': 'NM',
+        'New York': 'NY',
+        'North Carolina': 'NC',
+        'North Dakota': 'ND',
+        'Northern Mariana Islands':'MP',
+        'Ohio': 'OH',
+        'Oklahoma': 'OK',
+        'Oregon': 'OR',
+        'Pennsylvania': 'PA',
+        'Puerto Rico': 'PR',
+        'Rhode Island': 'RI',
+        'South Carolina': 'SC',
+        'South Dakota': 'SD',
+        'Tennessee': 'TN',
+        'Texas': 'TX',
+        'Utah': 'UT',
+        'Vermont': 'VT',
+        'Virgin Islands': 'VI',
+        'Virginia': 'VA',
+        'Washington': 'WA',
+        'West Virginia': 'WV',
+        'Wisconsin': 'WI',
+        'Wyoming': 'WY'
+    }
+
+    state_conv = us_state_abbrev[state]
+
+    return_list = [emp_id, first_name, last_name, dob_conv, ssn_conv, state_conv]
+    
+    return return_list
+
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+  
+    csv_header = next(csvreader)
+    with open(output_file, "w", newline="") as datafile:
+        writer = csv.writer(datafile)
+        writer.writerow(["Emp ID", "First Name", "Last Name", "DOB", "SSN", "State"])
+        for row in csvreader:
+            converted_row = convert_data(row)
+            writer.writerow(converted_row)
+
+        
